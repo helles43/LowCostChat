27db9b0bfd01c38ab78b9ef1341dc4f4df39da87
helles43 <egetaha20113@gmail.com>
2024-12-15 22:41:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,17 @@ app.use(express.static('public'));
 // Handle real-time messaging with Socket.io
 io.on('connection', (socket) => {
   console.log('A user connected');
-  
+
+  // Handle user logging in
+  socket.on('user-logged-in', (username) => {
+    console.log(`${username} has logged in`);
+  });
+
+  // Handle user logging out
+  socket.on('user-logged-out', (username) => {
+    console.log(`${username} has logged off`);
+  });
+
   // Send the new message to all users
   socket.on('new-message', (message) => {
     io.emit('new-message', message);
